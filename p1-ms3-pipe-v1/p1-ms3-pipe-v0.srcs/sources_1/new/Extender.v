9d623611e2e2d219655ba4bf77d6d921b5d6b67b
Marc <mark.nagy1@hotmail.com>
2019-04-14 15:44:52 +0200

    jalr corrected - not tested yet

---
+++ p1-ms3-pipe-v1/p1-ms3-pipe-v0.srcs/sources_1/new/Extender.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2019 03:44:22 PM
+// Design Name: 
+// Module Name: Extender
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Extender(
+    input [15:0] Inst_in,
+    output [31:0] Inst_out
+    );
+endmodule
